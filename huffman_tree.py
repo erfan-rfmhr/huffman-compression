e0609a314ef0698f49eb6c4273e8a0d846a5b06b
erfan <erfan.arefmehr@gmail.com>
2023-01-23 12:18:15 +0330

    initialize huffman class

--- huffman_tree.py
+++ huffman_tree.py
@@ -1,6 +1,15 @@
+import heapq
+import os
+
+
 class Node:
-    def __init__(self, value: str, frequency: int) -> None:
-        self.value = value
+    def __init__(self, char: str, frequency: int) -> None:
+        self.char = char
         self.frequency = frequency
         self.right = None
         self.left = None
+        
+class Huffman:
+    def __init__(self) -> None:
+        self.codes = {}
+        self.priority_queue = []
