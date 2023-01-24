0bf8945f68684e6b0f8463d642b9de81ef835be3
erfan <erfan.arefmehr@gmail.com>
2023-01-24 15:25:19 +0330

    update encode method

--- huffman_tree.py
+++ huffman_tree.py
@@ -100,37 +100,61 @@ class Huffman:
         # Recursively call the method for the right subtree
         self.__find_codes(root.right, current_code + '1')
         
-    # Encoding text into bytes
-    def encode_text(self, text: str) -> bytes:
-        """This function encodes the given text into bytes using a given code. 
-        
-        This function first finds the frequency of each character in the given string, 
-        builds a priority queue from the given frequencies and creates a tree from 
-        that queue. After the tree is created, it finds the codes for each character
-        and encodes the given text with the found codes.
+    def encode_text(self, text) -> bytes:
+        """Encodes the given text using Huffman encoding.
         
         Params:
-            text (str): The text to be encoded
+            text (str): The text to encode.
         
         Returns:
-            bytes: The encoded version of the given text
+            bytes: The encoded text.
         """
+        # Initialize the encoded text string
+        encoded_text = ''
+
+        # Count the frequencies of characters in the text
         frequencies = self.find_frequencies(text)
+
+        # Create a priority queue of tuples representing
+        # the characters and their frequencies
         self.__build_priority_queue(frequencies)
+
+        # Build the Huffman tree using the priority queue
         self.__build_tree()
+
+        # Calculate the Huffman codes
         self.__find_codes(self.root)
+        print(self.codes)
+
+        # Create a bytearray object to store the encoded output
         array = bytearray()
-        num_of_codes = len(self.codes)
-        array.append(num_of_codes)
-        # Append the ordinal of each character
-        for char, code in self.codes.items():
-            array.append(ord(char))
-            # Encode each character with a padding of 1
-            code = '1' + code
-            decimal_code = int(code, 2)
-            array.append(decimal_code)
-        # Append the encoded version of each character in the text    
+
+        # Calculate the number of characters (codes)
+        num_of_chars = len(frequencies)
+
+        # Encode the number of codes
+        encoded_text += bin(num_of_chars)[2:]
+
+        # Encode the character codes and their respective frequencies
+        for char, freq in frequencies.items():
+            encoded_text += bin(ord(char))[2:] + bin(freq)[2:]
+
+        # Encode the characters in the original text
         for char in text:
-            padded_encoded_char = '1' + self.codes[char]
-            array.append(int(padded_encoded_char, 2))
+            encoded_text += self.codes[char]
+
+        # Pad out the encoded text to a multiple of 8 bits
+        extra_padding = 8 - len(encoded_text) % 8
+        for i in range(extra_padding):
+            encoded_text += "0"
+
+        # Store the padding information at the beginning of the encoded text
+        padded_info = f"{extra_padding:08b}"
+        encoded_text = padded_info + encoded_text
+        # Convert the encoded text to bytes
+        for i in range(0, len(encoded_text), 8):
+            byte = encoded_text[i:i+8]
+            array.append(int(byte, 2))
+
+        # Return the bytes object
         return bytes(array)
